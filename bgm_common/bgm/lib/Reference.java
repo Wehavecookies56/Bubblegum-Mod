76de8e939a7915b79b478270fc208a057c781609
Toby Wishart <wehavecookies56b@gmail.com>
2013-05-15 20:38:03 +0100

    Where all information is referenced from

---
+++ bgm_common/bgm/lib/Reference.java
@@ -0,0 +1,8 @@
+package bgm.lib;
+
+public class Reference {
+
+    public static final String MOD_NAME = "Bubblegum Mod";
+    public static final String MOD_ID = "Wehavecookies56.bgm";
+    public static final String MOD_VER = "0.0.1 Alpha";
+}
