7e531ea2cec406515643d88c68c8afc63b805e4f
Toby Wishart <wehavecookies56b@gmail.com>
2013-05-15 20:36:57 +0100

    Base Class for Mod with no other things in

---
+++ bgm_common/bgm/BubblegumMod.java
@@ -0,0 +1,29 @@
+package bgm;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import bgm.lib.Reference;
+
+@Mod(name = Reference.MOD_NAME, modid = Reference.MOD_ID, version = Reference.MOD_VER)
+public class BubblegumMod {
+
+    @PreInit
+    public void preinit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
